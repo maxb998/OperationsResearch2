ba291814bf10694323f37b0bcaaea92fe20408ad
maxb998 <102738073+maxb998@users.noreply.github.com>
2023-10-28 18:54:10 +0200

    Fixed points generation

--- pyScripts/TspRandomInstance.py
+++ pyScripts/TspRandomInstance.py
@@ -4,10 +4,12 @@ import numpy as np
 from matplotlib import pyplot
 from datetime import datetime
 
+ptRange = 10000
+
 def main():
     n, dist_type, filename, seed = arg_parser()
 
-    f = open(filename, "w")
+    f = open(filename, "a")
     
     f.write("NAME : {0}\n".format(os.path.splitext(os.path.basename(filename))[0]))
     f.write("COMMENT : Randomly generated tsp instance using seed = {0}\n".format(seed))
@@ -22,9 +24,19 @@ def main():
     # random points within range [0, 1)
     points = np.random.rand(n, 2)
 
-    # set the points to have range [0, 10000)
-    points = points * 10000
+    # set the points to have range [0, ptRange)
+    points = points * ptRange
 
+    # check that all points are different
+    i = 1
+    while i < n:
+        if all(np.isin(points[i], points[0:i-1])):
+            points[i] = np.random.rand(2) * ptRange
+            i -= 1
+            break
+        i += 1
+
+    # write points
     for i in range(n):
         f.write("{0} {1} {2}\n".format(i+1, points[i,0], points[i,1]))
 
@@ -43,9 +55,9 @@ def arg_parser() -> tuple[ int, str, str, int]:
     # parse arguments
     parser = argparse.ArgumentParser(prog='TspRandomInstance', epilog='Tsp Random Instance Generator', description='Generate random instance of a traveling salesman problem and saves it according to tsplib specifications.')
     parser.add_argument('number_of_nodes', type=int, help='Number of nodes that the newly generated instance will contain.')
-    parser.add_argument('distance_type', choices=["EUC_2D","MAN_2D","MAX_2D", "ATT"], help='Type of distance used. Supported types only.')
+    parser.add_argument('distance_type', choices=["EUC_2D","MAN_2D","MAX_2D", "CEIL_2D", "ATT"], help='Type of distance used. Supported types only.')
     parser.add_argument('-o', '--output_file', metavar='OUTPUT_FILENAME')
-    parser.add_argument('-s', '--seed', metavar='SEED', type=int, help='Set random seed manually. (use time in seconds otherwise)')
+    parser.add_argument('-s', '--seed', metavar='SEED', type=int, help='Set random seed manually')
 
     args = parser.parse_args()
     
@@ -66,7 +78,7 @@ def arg_parser() -> tuple[ int, str, str, int]:
     
     # set output file
     if args.output_file == None:
-        filename = "data/{0}-{1}-{2}.tsp".format(n, dist_type, seed)
+        filename = "{0}-{1}-{2}.tsp".format(n, dist_type, seed)
     else:
         filename = args.output_file
     
